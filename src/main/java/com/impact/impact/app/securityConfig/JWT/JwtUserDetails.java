cdd746431683e5d2c649ca6be78c320ab5e5d2aa
dan.velescu <dan.velescu@xcode.ai>
2021-11-27 16:27:05 +0200

    commit v2 auth

--- src/main/java/com/impact/impact/app/securityConfig/JWT/JwtUserDetails.java
+++ src/main/java/com/impact/impact/app/securityConfig/JWT/JwtUserDetails.java
@@ -1,10 +1,6 @@
 package com.impact.impact.app.securityConfig.JWT;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
